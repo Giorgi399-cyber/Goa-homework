3d76fe39521e27e5454d6db5350049fe1496a66f
Giorgi399-cyber <wiklauri259@gmail.com>
2025-05-01 14:52:35 +0400

    second commit

---
+++ day 04/classwork/main.py
@@ -0,0 +1,21 @@
+#print"Lika"-აქ საჭიროა ფრჩხილებში ჩასმა რადგან პრინტ ფუნქციას სჭირდება ბრჭყალები.
+print("Lika")
+
+
+#analyst_salary=3000
+#print(Analyst_salary)-ეს ფორმა არასწორია რადგან თუ ჩვენ გვინდა რომ ტერმინალში გამოიტანოს სწორი ინფო მაშინ ჩვენ ყველაფერი ზუსტად უნდა გადავიტანოთ პრინტ ფუნქციაში.print(analyst_salary)
+analyst_salary=3000
+print(analyst_salary)
+
+#n1=27
+#n2="12
+#print(n1/n2)-ეს ფორმა არასწორია რადგან n2 cvladi არასწორადაა შევსებული.
+n1=27
+n2=12
+print(n1/n2)
+
+
+#Status=Mentor
+#print(status)-aklia brWyalebi.
+Status="Mentor"
+print(Status)
